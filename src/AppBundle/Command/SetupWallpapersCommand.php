d3ad9cb8ddacd1f493746dde790df51d136c9a63
cemilcelik <cemilcelik84@gmail.com>
2017-10-17 12:21:44 +0300

    Command app:setup-wallpapers visualize finish.

--- src/AppBundle/Command/SetupWallpapersCommand.php
+++ src/AppBundle/Command/SetupWallpapersCommand.php
@@ -5,6 +5,8 @@ namespace AppBundle\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Helper\Table;
 use Doctrine\ORM\EntityManagerInterface;
 use AppBundle\Entity\Wallpaper;
 
@@ -31,8 +33,16 @@ class SetupWallpapersCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $io = new SymfonyStyle($input, $output);
+
         $wallpapers = glob($this->rootDir . '/../web/images/*.*');
+        $wallpapersCount = count($wallpapers);
+
+        $io->title('Importing Wallpapers');
+        $io->progressStart($wallpapersCount);
         
+        $filenames = [];
+
         foreach ($wallpapers as $wallpaper) {
             [
                 'basename' => $filename,
@@ -50,11 +60,24 @@ class SetupWallpapersCommand extends Command
                 ->setWidth($width);
             
             $this->em->persist($wp);
+
+            $filenames[] = [ $filename ];
+
+            $io->progressAdvance();
         }
 
         $this->em->flush();
 
-        $output->writeln('Command result.');
+        $io->progressFinish();
+
+        $table = new Table($output);
+        $table
+            ->setHeaders(['Filenames'])
+            ->setRows($filenames)
+        ;
+        $table->render();
+
+        $io->success(sprintf('Cool, we added &d wallpapers.', $wallpapersCount));
     }
 
 }
